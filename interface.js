f3e8741a96edf71c0f46e3c20113464bc331a8c1
DarkChocLatte <darkchoclatte@gmail.com>
2017-01-19 11:51:51 +0000

    changes

--- interface.js
+++ interface.js
@@ -4,8 +4,7 @@ $( document ).ready(function() {
   var sqDiagram = new SqDiagram();
 
   $('.class-class, .method-class, .collaborator-class').on('input', function (event) {
-  event.preventDefault();
-  $('#diagram').html('')
+  $('#diagram').html('') /////////////////////////////////
   var name = updateDiagram();
   sqDiagram.drawSomething(name);
 })
